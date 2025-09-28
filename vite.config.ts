62bec5b12d8d11b337dd9b88a92850818fb0e036
Roman Burlakov <r.burlakov@mail.astondevs.ru>
2025-09-28 14:35:54 +0300

    feat deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: 'https://github.com/L1keTheMoon/SaaSoft-test/',
   resolve: {
     alias: {
       '@': '/src',
